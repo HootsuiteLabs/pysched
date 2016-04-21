efd20277466bbfab43fdadeb09df58faa1ca5076
Rei Colina <reinaldo13@gmail.com>
2016-04-20 22:46:02 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pysched',
+      version='0.1',
+      description='Scheduling helper. Find the next available time slot given a set of sheduled items and predefined settings.',
+      url='https://github.com/HootsuiteLabs/pysched',
+      author='Reinaldo Colina',
+      author_email='reinaldo13+github@gmail.com',
+      license='MIT',
+      packages=['pysched'],
+      zip_safe=False)
